8e6a443b5e3fb707e1260a99761a9cbcaeeff958
SaiSri Angajala <angajalasaisri2000@gmail.com>
2022-06-02 16:20:00 +0530

    Added the functions and object array files.

---
+++ array_of_objects.js
@@ -0,0 +1,23 @@
+// Usally we encounter the objects array.
+// let's create a todo array, in which each task is a object
+let todo = [
+  {
+    id: 1,
+    task: 'Learn Java Script',
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    task: 'Learn SolidWorks',
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    task: 'Daily task of Internship',
+    isCompleted: true,
+  },
+]
+
+console.log(todo);
+console.log(todo[1].task);
+console.log(todo[2].isCompleted);
---
+++ function.js
@@ -0,0 +1,13 @@
+// In Js function keyword is used to declare the function
+function Greet(Name) {
+    console.log('Hello ' + Name);
+}
+Greet('SaiSri');
+/*Arrow function*/
+const Greet2=(Name)=>
+{
+    console.log("Hello " + Name);
+} 
+Greet2("Yamuna");
+console.log(Greet('Anuradha'));
+/*If there is no return statement, or function doesn't encounter return statement, then it returns the undefined.*/
