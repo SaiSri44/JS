e167b500ee45ec031aad2fb2745b77f6b4dd3e43
SaiSri Angajala <angajalasaisri2000@gmail.com>
2022-06-02 12:34:20 +0530

    Added the Data types Information

---
+++ intro.js
@@ -0,0 +1,4 @@
+console.log("SaiSri");
+console.log("Srinivasa Rao");
+console.log("Anuradha");
+console.log("Yamuna"); ""
---
+++ variable_and_constants.js
@@ -0,0 +1,30 @@
+/*variables has same meaning like other programming languages
+var,let keywords are used to declare the variables in js
+There are some issues with the var, using let is the best practice.
+*/
+
+/*NOTE : Semilicon is optional*/
+
+let Name = 'SaiSri Angajala';
+console.log(Name);
+
+/*Rules for Naming keyword : 
+
+1. They cannot be the keywords
+2. Should be meaningful
+3.They cannot start with the Number
+4.They cannot contain space or (-)
+5.They are case sensitive
+6.Use cananon notation (First letter of firs word is lower case and first letter for the rest of the words is Upper case ex: firstName, lastName, myName,viratKohli, rohitSharma)
+
+*/
+
+/*Decalring Multiple variables*/
+let fatherName = "Srinivasa Rao", motherName = "Anuradha";
+
+
+/*---------------CONSTANTS-------------------------
+constants are variables whose value is fixed, there value cannot be changed. If we try to assign a new value, it raises a error.
+*/
+const sisterName = "Yamuna Angajala";
+
