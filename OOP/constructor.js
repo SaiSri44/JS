73c52cb6e280358dc009a8b3a5e77b20df3bcbea
SaiSri Angajala <angajalasaisri2000@gmail.com>
2022-06-04 20:15:35 +0530

    Added gettitle method to constructor function in arrow function notation

--- OOP/constructor.js
+++ OOP/constructor.js
@@ -1,10 +1,13 @@
-//constructor funciton
+//constructor funcitons
 function book(title, author, year) {
     this.title = title;
     this.author = author;
     this.year = year;
     this.summary = function () {
         return `${this.title} was written by the ${this.author} in the year ${this.year}`;
+    }
+    this.gettitle = () => {
+        return this.title;
     }
 }
 
@@ -15,3 +18,5 @@ console.log(book2.author);
 // /new keyword is used to create the object of the class or constructor function in java script 
 console.log(book1.summary());
 console.log(book2.summary());
+console.log(book1.gettitle());
+console.log(book2.gettitle()); 
